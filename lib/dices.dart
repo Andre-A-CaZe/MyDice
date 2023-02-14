467edf833ed3ede2208f7bcd7f1ab04e59a8dfdb
Andre Cadena <al19760606@ite.edu.mx>
2023-02-13 21:02:53 -0800

    Version 1.3 Boton Randomizador

--- lib/dices.dart
+++ lib/dices.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
+
+import 'package:flutter/material.dart';
 
 class MyDices extends StatefulWidget {
   const MyDices({super.key});
@@ -9,51 +10,25 @@ class MyDices extends StatefulWidget {
 }
 
 class _MyDicesState extends State<MyDices> {
-  String one = 'assets/img/dice1.png';
-  String two = 'assets/img/dice1.png';
-
-  void diceChange() {
-    setState(() {
-      int selection = Random().nextInt(6) + 1;
-      one = 'assets/img/dice$selection.png';
-      selection = Random().nextInt(6) + 1;
-      two = 'assets/img/dice$selection.png';
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const SizedBox(
-          height: 70,
+    return Row(children: [
+      Dado(Random().nextInt(6) + 1),
+      Dado(Random().nextInt(6) + 1)
+    ]);
+  }
+
+  Expanded Dado(valorDado) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          setState(() {});
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.asset('assets/img/dice$valorDado.png'),
         ),
-        Row(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset(one),
-              ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset(two),
-              ),
-            )
-          ],
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        TextButton(
-            onPressed: diceChange,
-            child: const Text(
-              "Rodar Dado",
-              style: TextStyle(color: Colors.black, fontSize: 30),
-            ))
-      ],
+      ),
     );
   }
 }
